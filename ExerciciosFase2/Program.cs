4ceb0cd82f782885bed62e0c043e4a8b97176aec
Italo Biondo <italo.a.biondo@gmail.com>
2024-01-11 19:58:59 -0300

    3

---
+++ ExerciciosFase2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("## Exercícios Fase 2 ##\n");
+
+Console.WriteLine("\nDigite o seu nome: ");
+string? nome = Console.ReadLine();
+int idade = 32;
+double nota = 7.5;
+
+Console.WriteLine($"\nAluno {nome} tem {idade} anos e a sua nota foi {nota} pontos.");
+Console.WriteLine($"\nNome: {nome}\nIdade: {idade}\nNota: {nota}");
+
+Console.ReadKey();
